282b4bac525cdc600678f21b76d9e7e0da3a21bb
RubenGomez1990 <rubengomez@paucasesnovescifp.cat>
2026-03-13 15:36:26 +0100

    funciones sql db_provider

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -59,12 +59,72 @@ class DBProvider {
     return res; // Devuelve el ID del último registro insertado
   }
 
-  // Hace lo mismo pero utilizando la librería sqflite.
+  // Hace lo mismo pero utilizando la librería sqflite y no en crudo.
   Future<int> insertScan(ScanModel scanNuevo) async {
     final db = await database;
     //Pide el nombre de la tabla scan y un mapa
     final res = await db.insert('Scans', scanNuevo.toMap());
     print(res);
+    return res;
+  }
+
+  // Recupera la lista de scans. El future tiene que recibir la lista futura.
+  Future<List<ScanModel>> getAllScans() async {
+    final db = await database;
+    final res = await db.query('Scans');
+    return res.isNotEmpty
+        ? res.map((e) => ScanModel.fromMap(e)).toList()
+        : []; // Si res no está vacío, mapeamos cada fila a una lista, si no la devuelve vacía
+  }
+
+  // Devuelve un Scan por su ID
+  Future<ScanModel?> getScanById(int id) async {
+    final db = await database;
+    final res = await db.query('Scans', where: 'id = ?', whereArgs: [
+      id
+    ]); // Where (condición), whereArgs (el argumento que utiliza para la condición)
+
+    if (res.isNotEmpty) {
+      return ScanModel.fromMap(res
+          .first); // devuelve el primer elemento de la lista que tenga el argumento id
+    } else {
+      return null;
+    }
+  }
+
+  // Devuelve una lista de Scans por tipus.
+  Future<List<ScanModel>> getScanByTipus(String tipus) async {
+    // Importante indicar que es una lista futura.
+    final db = await database;
+    final res = await db.query('Scans', where: 'tipus = ?', whereArgs: [tipus]);
+
+    return res.isNotEmpty
+        ? res.map((e) => ScanModel.fromMap(e)).toList()
+        : []; // Hacemos la condición así porque tiene que mapearlo a una lista.
+  }
+
+  // Actualiza un Scan recibiendo por parámetro un scan nuevo.
+  Future<int> updateScan(ScanModel scanNuevo) async {
+    final db = await database;
+    final res = db.update('Scans', scanNuevo.toMap(),
+        where: 'id = ?', whereArgs: [scanNuevo.id]);
+
+    return res;
+  }
+
+  // Borra un scan por su id. Como solo es una operación de borrado devolverá el resultado final de filas borradas.
+  Future<int> deleteById(int id) async {
+    final db = await database;
+    final res = await db.delete('Scans', where: 'id = ?', whereArgs: [id]);
+    return res;
+  }
+
+  // Borra todos los scans
+  Future<int> deleteAllScans() async {
+    final db = await database;
+    final res = db.rawDelete('''
+    DELETE FROM Scans
+  ''');
     return res;
   }
 }
